3f97b8db1fc5b0ba0b9687dbd61920b6daf7e10c
dalikk <dmitriev.alik1999@gmail.com>
2024-11-05 23:48:13 +0900

    chore: remake product card data showing

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import Header from '@/app/components/Header';
 import ProductCard from '@/app/components/ProductCard';
+import data from '@/app/utils/data';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <div className="products-wrapper flex w-full flex-wrap justify-between gap-4 px-[10px] pt-[30px]">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {data.map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
       </div>
     </div>
   );
